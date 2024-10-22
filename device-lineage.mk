3afa6e5b34e8407e3a99f69d3e60306a3273b648
Libra420T <ryantf420@gmail.com>
2024-10-22 10:44:56 -0400

    Initial Commit

---
+++ device-lineage.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
+# ANGLE - Almost Native Graphics Layer Engine
+PRODUCT_PACKAGES += \
+    ANGLE
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/google/shusky-kernel/Image.lz4
+
+# PowerShare
+include hardware/google/pixel/powershare/device.mk
+
+# wireless_charger HAL service
+include device/google/gs-common/wireless_charger/wireless_charger.mk
+
+# Build necessary packages for vendor
+
+# Codec2
+PRODUCT_PACKAGES += \
+    libexynosv4l2
+
+# Graphics
+PRODUCT_PACKAGES += \
+    libEGL_angle \
+    libGLESv1_CM_angle \
+    libGLESv2_angle
+
+# Identity credential
+PRODUCT_PACKAGES += \
+    android.hardware.identity_credential.xml
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.dynamic_sensor_hal
